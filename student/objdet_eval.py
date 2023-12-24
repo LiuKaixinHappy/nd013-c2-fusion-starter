734de0d085ba321e5c746aa1316f02874ccfca04
Liukaixin <liukaixin@speechocean.com>
2023-12-24 19:37:43 +0800

    finish mid term project

--- student/objdet_eval.py
+++ student/objdet_eval.py
@@ -13,7 +13,7 @@
 # general package imports
 import numpy as np
 import matplotlib
-matplotlib.use('wxagg') # change backend so that figure maximizing works on Mac as well     
+# matplotlib.use('wxagg') # change backend so that figure maximizing works on Mac as well
 import matplotlib.pyplot as plt
 
 import torch
@@ -23,6 +23,7 @@ from operator import itemgetter
 # add project directory to python path to enable relative imports
 import os
 import sys
+
 PACKAGE_PARENT = '..'
 SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
@@ -31,72 +32,115 @@ sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 import misc.objdet_tools as tools
 
 
+def calculate_iou(gt_bbox, pred_bbox):
+    """
+    calculate iou
+    args:
+    - gt_bbox [array]: 1x4 single gt bbox
+    - pred_bbox [array]: 1x4 single pred bbox
+    returns:
+    - iou [float]: iou between 2 bboxes
+    """
+    xmin = np.max([gt_bbox[0], pred_bbox[0]])
+    ymin = np.max([gt_bbox[1], pred_bbox[1]])
+    xmax = np.min([gt_bbox[2], pred_bbox[2]])
+    ymax = np.min([gt_bbox[3], pred_bbox[3]])
+    print(xmin, ymin, xmax, ymax)
+    intersection = max(0, xmax - xmin) * max(0, ymax - ymin)
+    gt_area = (gt_bbox[2] - gt_bbox[0]) * (gt_bbox[3] - gt_bbox[1])
+    pred_area = (pred_bbox[2] - pred_bbox[0]) * (pred_bbox[3] - pred_bbox[1])
+
+    union = gt_area + pred_area - intersection
+    print(intersection, union)
+    return intersection / union
+
+
 # compute various performance measures to assess object detection
 def measure_detection_performance(detections, labels, labels_valid, min_iou=0.5):
-    
-     # find best detection for each valid label 
-    true_positives = 0 # no. of correctly detected objects
+    # find best detection for each valid label
+    true_positives = 0  # no. of correctly detected objects
     center_devs = []
     ious = []
     for label, valid in zip(labels, labels_valid):
         matches_lab_det = []
-        if valid: # exclude all labels from statistics which are not considered valid
-            
+        if valid:  # exclude all labels from statistics which are not considered valid
+
             # compute intersection over union (iou) and distance between centers
 
-            ####### ID_S4_EX1 START #######     
+            ####### ID_S4_EX1 START #######
             #######
             print("student task ID_S4_EX1 ")
 
             ## step 1 : extract the four corners of the current label bounding-box
-            
+            box = label.box
+            label_corners = tools.compute_box_corners(box.center_x, box.center_y, box.width, box.length, box.heading)
+            print('label_corners', label_corners)
+            match_box = None
             ## step 2 : loop over all detected objects
-
+            for detection in detections:
                 ## step 3 : extract the four corners of the current detection
-                
+                pred_corners = tools.compute_box_corners(detection[1].item(),
+                                                         detection[2].item(),
+                                                         detection[5].item(),
+                                                         detection[6].item(),
+                                                         detection[7].item())
+                print('pred_corners', pred_corners)
                 ## step 4 : computer the center distance between label and detection bounding-box in x, y, and z
-                
+                pred_center = np.array([detection[1], detection[2], detection[3]])
+                label_center = np.array([box.center_x, box.center_y, box.center_z])
+                center_dist = label_center - pred_center
+                print('center_dist', center_dist)
                 ## step 5 : compute the intersection over union (IOU) between label and detection bounding-box
-                
+                iou = calculate_iou([label_corners[1][0], label_corners[1][1],
+                                     label_corners[3][0], label_corners[3][1]],
+                                    [pred_corners[1][0], pred_corners[1][1],
+                                     pred_corners[3][0], pred_corners[3][1]])
+                print('iou', iou)
                 ## step 6 : if IOU exceeds min_iou threshold, store [iou,dist_x, dist_y, dist_z] in matches_lab_det and increase the TP count
-                
+                if iou > min_iou:
+                    match_box = [iou, center_dist[0], center_dist[1], center_dist[2]]
+                    matches_lab_det.append(match_box)
+                    true_positives += 1
             #######
-            ####### ID_S4_EX1 END #######     
-            
+            ####### ID_S4_EX1 END #######
+
         # find best match and compute metrics
         if matches_lab_det:
-            best_match = max(matches_lab_det,key=itemgetter(1)) # retrieve entry with max iou in case of multiple candidates   
+            best_match = max(matches_lab_det,
+                             key=itemgetter(1))  # retrieve entry with max iou in case of multiple candidates
             ious.append(best_match[0])
             center_devs.append(best_match[1:])
 
-
-    ####### ID_S4_EX2 START #######     
+    ####### ID_S4_EX2 START #######
     #######
     print("student task ID_S4_EX2")
-    
+
     # compute positives and negatives for precision/recall
-    
+
     ## step 1 : compute the total number of positives present in the scene
-    all_positives = 0
+    all_positives = len(detections)
 
     ## step 2 : compute the number of false negatives
-    false_negatives = 0
+    valid_label_cnt = 0
+    for each in labels_valid:
+        if each:
+            valid_label_cnt += 1
+    false_negatives = valid_label_cnt - true_positives
 
     ## step 3 : compute the number of false positives
-    false_positives = 0
-    
+    false_positives = all_positives - true_positives
+
     #######
-    ####### ID_S4_EX2 END #######     
-    
+    ####### ID_S4_EX2 END #######
+
     pos_negs = [all_positives, true_positives, false_negatives, false_positives]
     det_performance = [ious, center_devs, pos_negs]
-    
+
     return det_performance
 
 
 # evaluate object detection performance based on all frames
 def compute_performance_stats(det_performance_all):
-
     # extract elements
     ious = []
     center_devs = []
@@ -105,22 +149,28 @@ def compute_performance_stats(det_performance_all):
         ious.append(item[0])
         center_devs.append(item[1])
         pos_negs.append(item[2])
-    
-    ####### ID_S4_EX3 START #######     
-    #######    
+
+    ####### ID_S4_EX3 START #######
+    #######
     print('student task ID_S4_EX3')
 
     ## step 1 : extract the total number of positives, true positives, false negatives and false positives
-    
+    pos_negs_arr = np.asarray(pos_negs)
+    positives = sum(pos_negs_arr[:, 0])
+    true_positives = sum(pos_negs_arr[:, 1])
+    false_negatives = sum(pos_negs_arr[:, 2])
+    false_positives = sum(pos_negs_arr[:, 3])
+    print("TP = " + str(true_positives) + ", FP = " + str(false_positives) + ", FN = " + str(false_negatives))
+
     ## step 2 : compute precision
-    precision = 0.0
+    precision = true_positives / (true_positives + false_positives)
 
-    ## step 3 : compute recall 
-    recall = 0.0
+    ## step 3 : compute recall
+    recall = true_positives / (true_positives + false_negatives)
 
-    #######    
-    ####### ID_S4_EX3 END #######     
-    print('precision = ' + str(precision) + ", recall = " + str(recall))   
+    #######
+    ####### ID_S4_EX3 END #######
+    print('precision = ' + str(precision) + ", recall = " + str(recall))
 
     # serialize intersection-over-union and deviations in x,y,z
     ious_all = [element for tupl in ious for element in tupl]
@@ -133,7 +183,6 @@ def compute_performance_stats(det_performance_all):
             devs_x_all.append(dev_x)
             devs_y_all.append(dev_y)
             devs_z_all.append(dev_z)
-    
 
     # compute statistics
     stdev__ious = np.std(ious_all)
@@ -147,15 +196,22 @@ def compute_performance_stats(det_performance_all):
 
     stdev__devz = np.std(devs_z_all)
     mean__devz = np.mean(devs_z_all)
-    #std_dev_x = np.std(devs_x)
+    # std_dev_x = np.std(devs_x)
 
     # plot results
     data = [precision, recall, ious_all, devs_x_all, devs_y_all, devs_z_all]
-    titles = ['detection precision', 'detection recall', 'intersection over union', 'position errors in X', 'position errors in Y', 'position error in Z']
+    titles = ['detection precision', 'detection recall', 'intersection over union', 'position errors in X',
+              'position errors in Y', 'position error in Z']
     textboxes = ['', '', '',
-                 '\n'.join((r'$\mathrm{mean}=%.4f$' % (np.mean(devs_x_all), ), r'$\mathrm{sigma}=%.4f$' % (np.std(devs_x_all), ), r'$\mathrm{n}=%.0f$' % (len(devs_x_all), ))),
-                 '\n'.join((r'$\mathrm{mean}=%.4f$' % (np.mean(devs_y_all), ), r'$\mathrm{sigma}=%.4f$' % (np.std(devs_y_all), ), r'$\mathrm{n}=%.0f$' % (len(devs_x_all), ))),
-                 '\n'.join((r'$\mathrm{mean}=%.4f$' % (np.mean(devs_z_all), ), r'$\mathrm{sigma}=%.4f$' % (np.std(devs_z_all), ), r'$\mathrm{n}=%.0f$' % (len(devs_x_all), )))]
+                 '\n'.join((r'$\mathrm{mean}=%.4f$' % (np.mean(devs_x_all),),
+                            r'$\mathrm{sigma}=%.4f$' % (np.std(devs_x_all),),
+                            r'$\mathrm{n}=%.0f$' % (len(devs_x_all),))),
+                 '\n'.join((r'$\mathrm{mean}=%.4f$' % (np.mean(devs_y_all),),
+                            r'$\mathrm{sigma}=%.4f$' % (np.std(devs_y_all),),
+                            r'$\mathrm{n}=%.0f$' % (len(devs_x_all),))),
+                 '\n'.join((r'$\mathrm{mean}=%.4f$' % (np.mean(devs_z_all),),
+                            r'$\mathrm{sigma}=%.4f$' % (np.std(devs_z_all),),
+                            r'$\mathrm{n}=%.0f$' % (len(devs_x_all),)))]
 
     f, a = plt.subplots(2, 3)
     a = a.ravel()
--- student/objdet_pcl.py
+++ student/objdet_pcl.py
@@ -14,10 +14,14 @@
 import cv2
 import numpy as np
 import torch
+import zlib
+import open3d as o3d
+import matplotlib.pyplot as plt
 
 # add project directory to python path to enable relative imports
 import os
 import sys
+
 PACKAGE_PARENT = '..'
 SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
@@ -32,130 +36,177 @@ import misc.objdet_tools as tools
 
 # visualize lidar point-cloud
 def show_pcl(pcl):
-
-    ####### ID_S1_EX2 START #######     
+    ####### ID_S1_EX2 START #######
     #######
     print("student task ID_S1_EX2")
 
+    def key_callback(key):
+        vis.destroy_window()
+
     # step 1 : initialize open3d with key callback and create window
-    
+    vis = o3d.visualization.VisualizerWithKeyCallback()
+    vis.create_window()
     # step 2 : create instance of open3d point-cloud class
-
+    pcd = o3d.geometry.PointCloud()
     # step 3 : set points in pcd instance by converting the point-cloud into 3d vectors (using open3d function Vector3dVector)
-
+    pcd.points = o3d.utility.Vector3dVector(pcl[:, :3])
     # step 4 : for the first frame, add the pcd instance to visualization using add_geometry; for all other frames, use update_geometry instead
-    
+    vis.add_geometry(pcd)
     # step 5 : visualize point cloud and keep window open until right-arrow is pressed (key-code 262)
+    vis.register_key_callback(262, key_callback)
+    vis.run()
 
     #######
-    ####### ID_S1_EX2 END #######     
-       
+    ####### ID_S1_EX2 END #######
+
 
 # visualize range image
 def show_range_image(frame, lidar_name):
-
-    ####### ID_S1_EX1 START #######     
+    ####### ID_S1_EX1 START #######
     #######
     print("student task ID_S1_EX1")
 
     # step 1 : extract lidar data and range image for the roof-mounted lidar
-    
+    lidar = [obj for obj in frame.lasers if obj.name == lidar_name][0]
+
     # step 2 : extract the range and the intensity channel from the range image
-    
+    if len(lidar.ri_return1.range_image_compressed) > 0:
+        ri = dataset_pb2.MatrixFloat()
+        ri.ParseFromString(zlib.decompress(lidar.ri_return1.range_image_compressed))
+        ri = np.array(ri.data).reshape(ri.shape.dims)
     # step 3 : set values <0 to zero
-    
+    ri[ri < 0] = 0.0
     # step 4 : map the range channel onto an 8-bit scale and make sure that the full range of values is appropriately considered
-    
+    ri_range = ri[:, :, 0]
+    ri_range = ri_range * 255 / (np.amax(ri_range) - np.amin(ri_range))
+    img_range = ri_range.astype(np.uint8)
     # step 5 : map the intensity channel onto an 8-bit scale and normalize with the difference between the 1- and 99-percentile to mitigate the influence of outliers
-    
+    ri_intensity = ri[:, :, 1]
+    ri_intensity_8bit = np.clip(ri_intensity, 0, np.percentile(ri_intensity, 99))
+    ri_intensity_8bit = (ri_intensity_8bit - np.percentile(ri_intensity_8bit, 1)) / (
+                np.percentile(ri_intensity_8bit, 99) - np.percentile(ri_intensity_8bit, 1))
+    img_intensity = (ri_intensity_8bit * 255).astype(np.uint8)
     # step 6 : stack the range and intensity image vertically using np.vstack and convert the result to an unsigned 8-bit integer
-    
-    img_range_intensity = [] # remove after implementing all steps
+    stacked_image = np.vstack((img_range, img_intensity))
+    img_range_intensity = stacked_image.astype(np.uint8)
+
+    # crop +/-90 degree around the forward-facing x-axis.
+    #     deg90 = int(img_range_intensity.shape[1] / 4)
+    #     ri_center = int(img_range_intensity.shape[1] / 2)
+    #     img_range_intensity = img_range_intensity[:,ri_center-deg90:ri_center+deg90]
     #######
-    ####### ID_S1_EX1 END #######     
-    
+    ####### ID_S1_EX1 END #######
+
     return img_range_intensity
 
 
 # create birds-eye view of lidar data
 def bev_from_pcl(lidar_pcl, configs):
-
     # remove lidar points outside detection area and with too low reflectivity
     mask = np.where((lidar_pcl[:, 0] >= configs.lim_x[0]) & (lidar_pcl[:, 0] <= configs.lim_x[1]) &
                     (lidar_pcl[:, 1] >= configs.lim_y[0]) & (lidar_pcl[:, 1] <= configs.lim_y[1]) &
                     (lidar_pcl[:, 2] >= configs.lim_z[0]) & (lidar_pcl[:, 2] <= configs.lim_z[1]))
     lidar_pcl = lidar_pcl[mask]
-    
+
     # shift level of ground plane to avoid flipping from 0 to 255 for neighboring pixels
-    lidar_pcl[:, 2] = lidar_pcl[:, 2] - configs.lim_z[0]  
+    lidar_pcl[:, 2] = lidar_pcl[:, 2] - configs.lim_z[0]
 
     # convert sensor coordinates to bev-map coordinates (center is bottom-middle)
-    ####### ID_S2_EX1 START #######     
+    ####### ID_S2_EX1 START #######
     #######
     print("student task ID_S2_EX1")
 
     ## step 1 :  compute bev-map discretization by dividing x-range by the bev-image height (see configs)
-
-    ## step 2 : create a copy of the lidar pcl and transform all metrix x-coordinates into bev-image coordinates    
-
+    bev_discret = (configs.lim_x[1] - configs.lim_x[0]) / configs.bev_height
+    ## step 2 : create a copy of the lidar pcl and transform all metrix x-coordinates into bev-image coordinates
+    lidar_pcl_cpy = np.copy(lidar_pcl)
+    lidar_pcl_cpy[:, 0] = np.int_(np.floor(lidar_pcl_cpy[:, 0] / bev_discret))
     # step 3 : perform the same operation as in step 2 for the y-coordinates but make sure that no negative bev-coordinates occur
-
+    lidar_pcl_cpy[:, 1] = np.int_(np.floor(lidar_pcl_cpy[:, 1] / bev_discret) + (configs.bev_width + 1) / 2)
     # step 4 : visualize point-cloud using the function show_pcl from a previous task
-    
+    #     show_pcl(lidar_pcl_cpy)
     #######
-    ####### ID_S2_EX1 END #######     
-    
-    
+    ####### ID_S2_EX1 END #######
+
     # Compute intensity layer of the BEV map
-    ####### ID_S2_EX2 START #######     
+    ####### ID_S2_EX2 START #######
     #######
     print("student task ID_S2_EX2")
 
     ## step 1 : create a numpy array filled with zeros which has the same dimensions as the BEV map
-
+    intensity_map = np.zeros((configs.bev_height + 1, configs.bev_width + 1))
     # step 2 : re-arrange elements in lidar_pcl_cpy by sorting first by x, then y, then -z (use numpy.lexsort)
+    idx_intensity = np.lexsort((-lidar_pcl_cpy[:, 3], lidar_pcl_cpy[:, 1], lidar_pcl_cpy[:, 0]))
+    lidar_pcl_cpy = lidar_pcl_cpy[idx_intensity]
 
     ## step 3 : extract all points with identical x and y such that only the top-most z-coordinate is kept (use numpy.unique)
     ##          also, store the number of points per x,y-cell in a variable named "counts" for use in the next task
+    _, indices, counts = np.unique(lidar_pcl_cpy[:, 0:2], axis=0, return_index=True, return_counts=True)
+    print('counts', counts)
+    lidar_pcl_top = lidar_pcl_cpy[indices]
 
-    ## step 4 : assign the intensity value of each unique entry in lidar_top_pcl to the intensity map 
-    ##          make sure that the intensity is scaled in such a way that objects of interest (e.g. vehicles) are clearly visible    
+    ## step 4 : assign the intensity value of each unique entry in lidar_top_pcl to the intensity map
+    ##          make sure that the intensity is scaled in such a way that objects of interest (e.g. vehicles) are clearly visible
     ##          also, make sure that the influence of outliers is mitigated by normalizing intensity on the difference between the max. and min. value within the point cloud
+    #     b = np.array([0, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1, 1e+1, 1e+3, 1e+3, 1e+4, 1e+5, 1e+6, 1e+7])
+    #     hist,bins = np.histogram(lidar_pcl_int[:,3], bins=b)
+    #     print(hist)
+    # [   0    0    3  174 7586 5621  150   20    0    0    1    0    0]
+    idx_limit = lidar_pcl_top[:, 3] > 1
+    lidar_pcl_top[idx_limit, 3] = 1
+    intensity_map[np.int_(lidar_pcl_top[:, 0]), np.int_(lidar_pcl_top[:, 1])] = lidar_pcl_top[:, 3] / (
+                np.amax(lidar_pcl_top[:, 3]) - np.amin(lidar_pcl_top[:, 3]))
 
     ## step 5 : temporarily visualize the intensity map using OpenCV to make sure that vehicles separate well from the background
-
+    img_intensity = intensity_map * 256
+    img_intensity = img_intensity.astype(np.uint8)
+    cv2.imshow('img_intensity', img_intensity)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    # 使用Matplotlib库绘制热力图
+    plt.imshow(intensity_map, cmap='hot', interpolation='nearest')
+    plt.colorbar()
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.show()
     #######
-    ####### ID_S2_EX2 END ####### 
-
+    ####### ID_S2_EX2 END #######
 
     # Compute height layer of the BEV map
-    ####### ID_S2_EX3 START #######     
+    ####### ID_S2_EX3 START #######
     #######
     print("student task ID_S2_EX3")
 
     ## step 1 : create a numpy array filled with zeros which has the same dimensions as the BEV map
-
-    ## step 2 : assign the height value of each unique entry in lidar_top_pcl to the height map 
+    height_map = np.zeros((configs.bev_height + 1, configs.bev_width + 1))
+    ## step 2 : assign the height value of each unique entry in lidar_top_pcl to the height map
     ##          make sure that each entry is normalized on the difference between the upper and lower height defined in the config file
     ##          use the lidar_pcl_top data structure from the previous task to access the pixels of the height_map
+    height_map[np.int_(lidar_pcl_top[:, 0]), np.int_(lidar_pcl_top[:, 1])] = lidar_pcl_top[:, 2] / float(
+        np.abs(configs.lim_z[1] - configs.lim_z[0]))
 
     ## step 3 : temporarily visualize the intensity map using OpenCV to make sure that vehicles separate well from the background
+    img_height = height_map * 256
+    img_height = img_height.astype(np.uint8)
+    cv2.imshow('img_height', img_height)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
+    plt.clf()
+    plt.imshow(height_map, cmap='hot', interpolation='nearest')
+    plt.colorbar()
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.show()
     #######
-    ####### ID_S2_EX3 END #######       
-
-    # TODO remove after implementing all of the above steps
-    lidar_pcl_cpy = []
-    lidar_pcl_top = []
-    height_map = []
-    intensity_map = []
+    ####### ID_S2_EX3 END #######
 
     # Compute density layer of the BEV map
     density_map = np.zeros((configs.bev_height + 1, configs.bev_width + 1))
     _, _, counts = np.unique(lidar_pcl_cpy[:, 0:2], axis=0, return_index=True, return_counts=True)
-    normalizedCounts = np.minimum(1.0, np.log(counts + 1) / np.log(64)) 
+    normalizedCounts = np.minimum(1.0, np.log(counts + 1) / np.log(64))
     density_map[np.int_(lidar_pcl_top[:, 0]), np.int_(lidar_pcl_top[:, 1])] = normalizedCounts
-        
+
     # assemble 3-channel bev-map from individual maps
     bev_map = np.zeros((3, configs.bev_height, configs.bev_width))
     bev_map[2, :, :] = density_map[:configs.bev_height, :configs.bev_width]  # r_map
